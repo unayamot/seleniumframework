e4d549bf13b3f4e4b735a83f42ebf56a5325e9ec
unayamot <nayamotullah@gmail.com>
2019-04-13 14:37:34 -0400

    updated the locators in expediasearch

--- src/locators/ExpediaSearch.java
+++ src/locators/ExpediaSearch.java
@@ -1,11 +1,13 @@
 package locators;
 
 public class ExpediaSearch {
+	// sign in locators
 	public String accountLocator = "//*[@id=\"header-account-menu\"]";
 	public String signinLocator = "//*[@id=\"account-signin\"]";
 	public String locateID = "//*[@id=\"gss-signin-email\"]";
 	public String locatePass = "//*[@id=\"gss-signin-password\"]";
 	
+	// flight from locators
 	public String flyingFromLocator = "//*[@id=\"package-origin-hp-package\"]";
 	public String selectJFK = "//*[@id=\"aria-option-0\"]";
 }
